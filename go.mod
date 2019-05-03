bc6aab7697725f4318c78686585547e259cb9dbd
Ally Maumba <amaumba1@gmail.com>
2019-05-04 01:57:34 +0300

    added and modified microservices publish and subscribe

--- go.mod
+++ go.mod
@@ -1,6 +1,15 @@
 module github.com/amaumba1/eventbooking
 
 require (
+	github.com/Shopify/sarama v1.22.1
+	github.com/gorilla/handlers v1.4.0 // indirect
 	github.com/gorilla/mux v1.7.1
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/martin-helmich/cloudnativego-backend v0.0.0-20171222210257-6148b3bb66f2
+	github.com/mitchellh/mapstructure v1.1.2
+	github.com/prometheus/client_golang v0.9.2 // indirect
+	github.com/streadway/amqp v0.0.0-20190404075320-75d898a42a94
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- src/bookingservice/main.go
+++ src/bookingservice/main.go
@@ -2,62 +2,63 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"log"
 
-	"github.com/amaumba1/eventbooking/src/eventservice/rest"
+	"github.com/Shopify/sarama"
+	"github.com/amaumba1/eventbooking/src/bookingservice/listener"
+	"github.com/amaumba1/eventbooking/src/bookingservice/rest"
 	"github.com/amaumba1/eventbooking/src/lib/configuration"
-	"github.com/amaumba1/eventbooking/src/lib/persistence/dblayer"
 	"github.com/amaumba1/eventbooking/src/lib/msgqueue"
 	msgqueue_amqp "github.com/amaumba1/eventbooking/src/lib/msgqueue/amqp"
 	"github.com/amaumba1/eventbooking/src/lib/msgqueue/kafka"
+	"github.com/amaumba1/eventbooking/src/lib/persistence/dblayer"
 	"github.com/streadway/amqp"
 )
 
+func panicIfErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+	var eventListener msgqueue.EventListener
 	var eventEmitter msgqueue.EventEmitter
 
-	confPath := flag.String("conf", `.\configuration\config.json`, "flag to set the path to the configuration json file")
+	confPath := flag.String("conf", "./configuration/config.json", "flag to set the path to the configuration json file")
 	flag.Parse()
+
 	//extract configuration
 	config, _ := configuration.ExtractConfiguration(*confPath)
 
 	switch config.MessageBrokerType {
 	case "amqp":
 		conn, err := amqp.Dial(config.AMQPMessageBroker)
-		if err != nil {
-			panic(err)
-		}
+		panicIfErr(err)
+
+		eventListener, err = msgqueue_amqp.NewAMQPEventListener(conn, "events", "booking")
+		panicIfErr(err)
 
 		eventEmitter, err = msgqueue_amqp.NewAMQPEventEmitter(conn, "events")
-		if err != nil {
-			panic(err)
-		}
+		panicIfErr(err)
 	case "kafka":
 		conf := sarama.NewConfig()
 		conf.Producer.Return.Successes = true
 		conn, err := sarama.NewClient(config.KafkaMessageBrokers, conf)
-		if err != nil {
-			panic(err)
-		}
+		panicIfErr(err)
+
+		eventListener, err = kafka.NewKafkaEventListener(conn, []int32{})
+		panicIfErr(err)
 
 		eventEmitter, err = kafka.NewKafkaEventEmitter(conn)
-		if err != nil {
-			panic(err)
-		}
+		panicIfErr(err)
 	default:
 		panic("Bad message broker type: " + config.MessageBrokerType)
 	}
 
-	fmt.Println("Connecting to database")
 	dbhandler, _ := dblayer.NewPersistenceLayer(config.Databasetype, config.DBConnection)
 
-	fmt.Println("Serving API")
-	//RESTful API start
-	err := rest.ServeAPI(config.RestfulEndpoint, dbhandler, eventEmitter)
-	if err != nil {
-		panic(err)
-	}
+	processor := listener.EventProcessor{eventListener, dbhandler}
+	go processor.ProcessEvents()
+
+	rest.ServeAPI(config.RestfulEndpoint, dbhandler, eventEmitter)
 }
-
-
