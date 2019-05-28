52e472ad50b44c2d0abb78a7e379061df4c7a8a2
Ally Maumba <amaumba1@gmail.com>
2019-05-28 08:44:22 -0500

    basic api modification

--- src/eventservice/rest/rest.go
+++ src/eventservice/rest/rest.go
@@ -5,25 +5,16 @@ import (
 
 	"github.com/amaumba1/eventbooking/src/lib/persistence"
 
-	"github.com/amaumba1/eventbooking/src/lib/msgqueue"
-
 	"github.com/gorilla/mux"
 )
 
+func ServeAPI(endpoint string, dbHandler persistence.DatabaseHandler) error {
+    handler := newEventHandler(dbHandler)
+    r := mux.NewRouter()
+    eventsrouter := r.PathPrefix("/events").Subrouter()
+eventsrouter.Methods("GET").Path("/{SearchCriteria}/{search}").HandlerFunc(handler.findEventHandler)
+    eventsrouter.Methods("GET").Path("").HandlerFunc(handler.allEventHandler)
+    eventsrouter.Methods("POST").Path("").HandlerFunc(handler.newEventHandler)
 
-func ServeAPI(endpoint string, dbHandler persistence.DatabaseHandler, eventEmitter msgqueue.EventEmitter) error {
-	handler := newEventHandler(dbHandler, eventEmitter)
-
-	r := mux.NewRouter()
-	eventsrouter := r.PathPrefix("/events").Subrouter()
-	eventsrouter.Methods("GET").Path("/{SearchCriteria}/{search}").HandlerFunc(handler.findEventHandler)
-	eventsrouter.Methods("GET").Path("").HandlerFunc(handler.allEventHandler)
-	eventsrouter.Methods("GET").Path("/{eventID}").HandlerFunc(handler.oneEventHandler)
-	eventsrouter.Methods("POST").Path("").HandlerFunc(handler.newEventHandler)
-
-	locationRouter := r.PathPrefix("/locations").Subrouter()
-	locationRouter.Methods("GET").Path("").HandlerFunc(handler.allLocationsHandler)
-	locationRouter.Methods("POST").Path("").HandlerFunc(handler.newLocationHandler)
-
-	return http.ListenAndServe(endpoint, r)
+    return http.ListenAndServe(endpoint, r)
 }
